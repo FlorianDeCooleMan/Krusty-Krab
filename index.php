d48e4a959deebbf9ec95b8ebf0b08b0c25a130b7
FlorianDeCooleMan <120579373+FlorianDeCooleMan@users.noreply.github.com>
2023-03-20 13:08:22 +0100

    fotos toevoegen

--- index.php
+++ index.php
@@ -6,13 +6,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400&family=Kanit:ital,wght@1,300&display=swap" rel="stylesheet">
 </head>
 <body>
-  <div class="lucht"></div>
+  <header></header>
+  <div class="lucht">
+    <img src="Krusty Krab logo.png" alt="krab">
+    <h1>The krusty Krab</h1>
+  </div>
 <div class="ocean">
   <div class="wave"></div>
   <div class="wave"></div>
-  <div class="zee"></div>
+  <div class="zee">
+    <div class="main-tekst">
+      <h1>Restaurant the Krusty Krab</h1>
+      <p>
+      Restaurant The Krusty Krab in Bikini Bodem <br>
+      is een gemoedelijkbourgondisch restaurant waar u kunt genieten van dagverse gerechten. <br>
+      Ook met grotere gezelschappen kunt u bij ons <br>
+      terecht. <br>
+      Ons restaurant is ook geschikt voor zakelijke diners <br>
+      door met name de ruime opstellingen de goed <br>
+      akoestiek zodat u ongestoord kan communiceren. <br>
+      U kunt bij ons in het restaurant dineren en bij warm <br>
+      weer hebben wij een mooi terrasen een sfeervolle <br>
+      achtertuin waar gedineerd kan worden. <br> <br>
+      In overleg is veel mogelijk.
+      </p>
+    </div>
+    <div class="bloem1"></div>
+  <div class="bruin-vak">
+    <div class="genieten"><h1>Samen Genieten</h1></div>
+    <div class="eten-fotos">
+      <img src="glas.png" alt="glas" width="400px" height="400px">
+      <img src="pasta.png" alt="pasta" width="400px" height="400px">
+      <img src="brulee.png" alt="brulee" width="400px" height="400px">
+    </div>
+    <div class="menu-achtergrond"></div>
+  </div>
+  </div>
   <footer>
   </footer>
   
